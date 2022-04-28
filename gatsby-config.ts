f1ca986e022069eac36008cf039240cd0f86ec6e
Adam Doskhoev <Adam.Doskhoev@glidewelldental.com>
2022-04-28 18:37:38 +0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,18 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `My First Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
+
+export default config;
