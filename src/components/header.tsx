69bb1380a7149da31c96f285c6147a4507f75cd8
Adam Doskhoev <Adam.Doskhoev@glidewelldental.com>
2022-05-08 02:43:42 +0300

    another changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 import styled from 'styled-components'
 import { Navigation } from './navigation'
@@ -8,9 +9,26 @@ const SiteHeader = styled.header`
   width: 950px;
 `
 
+const Title = styled.h2`
+  background: #aabbcc;
+  padding: 20px;
+  margin: 0;
+`
+
 export const Header = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <SiteHeader>
+      <Title>{data.site.siteMetadata?.title}</Title>
       <Navigation />
     </SiteHeader>
   )
